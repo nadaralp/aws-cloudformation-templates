23c1afe8fc34e5b328c7d9fc6cd1a60f717ffbac
nadaralp <nadaralp16@gmail.com>
2021-10-31 10:06:38 +0200

    added test folder to test test nested stacks

--- personal/ci-cd/deploy.sh
+++ personal/ci-cd/deploy.sh
@@ -3,3 +3,4 @@
 # Exampe of deploying with multiple parameter overrides
 aws cloudformation deploy --template-file GitHubCiCdPipeline.yml --stack-name my-pipeline --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides ProjectName=nadar-ci-cd GitHubRepo=ci-cd-test GitHubOwner=nadar-pandologic TriggerBranch=master IsManualApprovalStepActive=true
+
